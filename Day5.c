a23e3ffbdbf75c41d914dcf068d5a54a980ecb7a
tejashrikamble365-debug <tejashrikamble365@gmail.com>
2025-08-19 22:14:53 +0530

    Update Day5.c

--- Day5.c
+++ Day5.c
@@ -5,20 +5,21 @@ void findLeaders(int arr[],int n){
   int max_from_right=arr[n-1];
   leaders[count++]=max_from_right;
 for(int i=n-2;i>=0;i--){
-  if(arr[i]>max-from_right){
-   max-from_right=arr[i];
+  if(arr[i]>max_from_right){
+   max_from_right=arr[i];
   leaders[count++]=arr[i];
 }
 }
 printf("Leaders: ");
     for(int i=count-1;i>=0;i--){
+      printf("%d",leaders[i]);
     }
 printf("\n");
 }
 int main(){
   int arr[]={16,17,4,3,5,2};
   int n=sizeof(arr)/sizeof(arr[0]);
-   findLeader(arr,n);
+   findLeaders(arr,n);
 return 0;
 }
   
