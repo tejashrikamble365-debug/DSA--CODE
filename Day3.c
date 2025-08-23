fb2f9038a2e31cd908f3c20cd60882f5d121d634
tejashrikamble365-debug <tejashrikamble365@gmail.com>
2025-08-23 12:45:53 +0530

    Create Day3.c

---
+++ Day3.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int findduplicate(int arr[],int n){
+   int slow=arr[0];
+   int fast=arr[0];
+do{
+slow=arr[slow];
+fast=arr[arr[fast]];
+}
+  while(slow!=fast);
+slow=arr[0];
+while(slow!=fast){
+slow=arr[slow];
+fast=arr[fast];
+}
+return slow;
+}
+int main(){
+  int arr[]={3,1,3,4,2};
+   int n=sizeof (arr)/sizeof(arr[0])-1;
+    int duplicate=findduplicate(arr,n);
+    printf("Duplicate number:%d\n",duplicate);
+   return 0;
+}
+
+
